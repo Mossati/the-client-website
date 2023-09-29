6de1d5e266e7de9cd1c16836969a33b7b7f9d24a
Mossati <xmossati9786@gmail.com>
2023-09-29 12:53:06 +0200

    code

---
+++ script/script.js
@@ -0,0 +1,17 @@
+function option1(){
+    document.getElementById('probleem').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+
+function option2(){
+    document.getElementById('waarombrain').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+
+function option3(){
+    document.getElementById('ervaringen').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
