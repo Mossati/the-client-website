51a6b007fce943dc5a32de0bc487485ff219a5b3
Mossati <xmossati9786@gmail.com>
2023-10-09 12:38:05 +0200

    code

--- script/script.js
+++ script/script.js
@@ -26,3 +26,58 @@ function classToggle(x) {
 
 document.querySelector('.nav-toggle')
   .addEventListener('click', classToggle);
+
+
+function readArticle1() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("section-more1");
+  var btnText = document.getElementById("btn-read");
+  
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lees verder";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lees minder";
+    moreText.style.display = "inline";
+  }
+}
+
+function readArticle2() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("section-more2");
+  var btnText = document.getElementById("btn-read");
+  
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lees verder";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lees minder";
+    moreText.style.display = "inline";
+  }
+}
+
+function readArticle3() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("section-more3");
+  var btnText = document.getElementById("btn-read");
+  
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lees verder";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lees minder";
+    moreText.style.display = "inline";
+  }
+}
+
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
